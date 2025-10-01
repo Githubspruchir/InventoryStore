1c6ceefcbcc6f0ffbfe4aca4d903fa0c5e850be9
Githubspruchir <spruchir@gmail.com>
2025-10-01 19:35:53 +0530

    Initial commit

---
+++ src/test/java/com/ruchir/InventoryStore/InventoryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruchir.InventoryStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
